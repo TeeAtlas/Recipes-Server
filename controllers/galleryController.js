e98afe78f45d5fd73f64ebdd4ae17fa0837b2e60
Tania Boterman <taniaboterman@gmail.com>
2024-02-11 21:24:37 +0100

    gallery fetching

--- controllers/galleryController.js
+++ controllers/galleryController.js
@@ -1,9 +1,20 @@
 import { pool } from '../db/pool.js';
 
+
+export const getAllGalleryItems = async (req, res) => {
+    try{
+        const { rows } = await pool.query(`SELECT * FROM gallery`);
+        res.json(rows);
+    } catch (error) {
+        console.error(`Sorry, no gallery here`, error);
+        res.sendStatus(500);
+    }
+}
+
 export const getGalleryItem = async (req, res) => {
     const { id } = req.params;
     try{
-        const { rows } = await pool.query(`SELECT * FROM gallery where id=$1`, [id]);
+        const { rows } = await pool.query(`SELECT * FROM gallery WHERE id=$1`, [id]); // Fixed SQL query
         if(rows.length === 0) {
             res.status(404).json({message: `Gallery object with ${id} not found`});
         } else {
--- routes/galleryRouter.js
+++ routes/galleryRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getGalleryItem } from '../controllers/galleryController.js';
+import { getAllGalleryItems, getGalleryItem } from '../controllers/galleryController.js';
 
 const galleryRouter = express.Router();
 
+galleryRouter.get('/', getAllGalleryItems);
 galleryRouter.get('/:id', getGalleryItem);
 
 export default galleryRouter;
